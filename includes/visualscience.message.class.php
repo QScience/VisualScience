b98354d4c50cbaed172fec97b19c4bd64f0e1d4b
Sébastien Arnold <seba-1511@2754171.no-reply.drupal.org>
2013-11-25 12:31:30 +0100

    First fixes for msg.class

--- includes/visualscience.message.class.php
+++ includes/visualscience.message.class.php
@@ -1,30 +1,33 @@
 <?php
 /**
- * @file(visualscience.message.class.php)
+ * @file (visualscience.message.class.php)
+ * 
  * The class that manages the backend to send a mail through the Drupal instance.
  */
 class visualscienceMessage {
 
 /**
  * Allows the sending of a mail through this Drupal instance
- * @return print 1 if everything went fine, else 0
+ * 
+ * @return int 
+ *   Print 1 if everything went fine, else 0
  */
-    public function visualscience_send_message() {
-        $subject =  check_plain($_POST['subject']);
-        $email = check_plain($_POST['recipients']['email']);
+public function visualscience_send_message() {
+  $subject =  check_plain($_POST['subject']);
+  $email = check_plain($_POST['recipients']['email']);
         // $name =  check_plain($_POST['recipients']['name']);
-        $message =  check_plain($_POST['message']);
+  $message =  check_plain($_POST['message']);
         $attachments =  $this->sanitizeArray($_POST['attachments']);// [0][0] will give the name of object n°0, while [0][1] will give its URL
         if ($attachments[0]) {
-            $attachmentsText = t('<br /><h3>Attached Files</h3>');
-            foreach ($attachments as $entry) {
-                $attachmentsText .= t('- <a href="' . $entry[1] . '" _target="blank">' . $entry[0] . '</a><br />');
-            }
+          $attachments_text = t('<br /><h3>Attached Files</h3>');
+          foreach ($attachments as $entry) {
+            $attachments_text .= t('- <a href="' . $entry[1] . '" _target="blank">' . $entry[0] . '</a><br />');
+          }
         }
         else {
-            $attachmentsText = '';
+          $attachments_text = '';
         }
-        $final_text = $message . check_plain($attachmentsText);
+        $final_text = $message . check_plain($attachments_text);
 
 
         global $user;
@@ -41,52 +44,55 @@ class visualscienceMessage {
         $message['subject'] = $subject;
         $message['body'] = $final_text;
 
-// Retrieve the responsible implementation for this message.
+        // Retrieve the responsible implementation for this message.
         $system = drupal_mail_system($module, $key);
 
-// Send e-mail.
+        // Send e-mail.
         $message['result'] = $system->mail($message);
-//If no errors, let's add file access to the user.
+        //If no errors, let's add file access to the user.
         if ($message['result'] == 1) {
-//getting user id from email.
-            $users = db_query_range('SELECT uid FROM {users} WHERE mail = :mail', 0, 1, array(':mail' => $email, ));
-//In the "impossible" case where two emails are the same in the db
-            $users = $users->fetchObject();
-            $uid = $users->uid;
-//actually adding the access to the user.
-            if (!is_null($uid) && isset($uid)) {
-                foreach ($attachments as $file) {
-                    $query = db_insert('visualscience_uploaded_files')->fields(array(
-                        'uid' => $uid,
-                        'email' => $from,
-                        'name' => $file[0],
-                        'url' => $file[1],
-                    ));
-                    $query->execute();
-                }
+          //getting user id from email.
+          $users = db_query_range('SELECT uid FROM {users} WHERE mail = :mail', 0, 1, array(':mail' => $email, ));
+          //In the "impossible" case where two emails are the same in the db
+          $users = $users->fetchObject();
+          $uid = $users->uid;
+          //actually adding the access to the user.
+          if (!is_null($uid) && isset($uid)) {
+            foreach ($attachments as $file) {
+              $query = db_insert('visualscience_uploaded_files')->fields(array(
+                'uid' => $uid,
+                'email' => $from,
+                'name' => $file[0],
+                'url' => $file[1],
+                ));
+              $query->execute();
             }
-            echo '1';
+          }
+          echo '1';
         }
         else {
-            echo '0';
+          echo '0';
         }
-    }
+      }
 
 /**
  * Sanitzes the content of an array recursively
- * @param  array $unSafeArray the array to sanitize.
- * @return array              the array sanitized
+ * @param  array $un_safe_array 
+ *   The array to sanitize.
+ *   
+ * @return array
+ *   The array sanitized
  */
-    private function sanitizeArray($unSafeArray) {
-        $safeArray = array();
-        foreach ($unSafeArray as $entry) {
-            if (gettype($entry) == 'array') {
-                array_push($safeArray, $this->sanitizeArray($entry));
-            }
-            else {
-                array_push($safeArray, check_plain($entry));
-            }
-        }
-        return $safeArray;
+protected function sanitizeArray($un_safe_array) {
+  $safe_array = array();
+  foreach ($un_safe_array as $entry) {
+    if (gettype($entry) == 'array') {
+      array_push($safe_array, $this->sanitizeArray($entry));
     }
+    else {
+      array_push($safe_array, check_plain($entry));
+    }
+  }
+  return $safe_array;
+}
 }
