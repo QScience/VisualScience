291b909cdb2c664d5696a0c40a0d22729e36f69b
Sébastien Arnold <seba-1511@2754171.no-reply.drupal.org>
2013-11-25 12:50:06 +0100

    SHould've fixed file msg.class

--- includes/visualscience.message.class.php
+++ includes/visualscience.message.class.php
@@ -3,16 +3,16 @@
  * @file
  * The class that manages sending a mail through the Drupal instance.
  */
-class VisualscienceMessage
-{
-  
+
+class VisualscienceMessage {
+
   /**
    * Allows the sending of a mail through this Drupal instance.
    * 
    * @return int 
    *   Print 1 if everything went fine, else 0.
    */
-  public function visualscience_send_message() {
+  public function visualscienceSendMessage() {
     $subject = check_plain($_POST['subject']);
     $email = check_plain($_POST['recipients']['email']);
     // $name =  check_plain($_POST['recipients']['name']);.
@@ -22,14 +22,14 @@ class VisualscienceMessage
     if ($attachments[0]) {
       $attachments_text = t('<br /><h3>Attached Files</h3>');
       foreach ($attachments as $entry) {
-        $attachments_text .= t('- <a href="' . $entry[1] . '" _target="blank">' . $entry[0] . '</a><br />');
+        $attachments_text .= '- <a href="' . $entry[1] . '" _target="blank">' . $entry[0] . '</a><br />';
       }
-    } else {
+    }
+    else {
       $attachments_text = '';
     }
     $final_text = $message . check_plain($attachments_text);
-    
-    
+
     global $user;
     $module = 'VisualScience';
     $key = uniqid('mail');
@@ -38,27 +38,27 @@ class VisualscienceMessage
     $from = $user->mail;
     $send = FALSE;
     $message = drupal_mail($module, $key, $email, $language, $params, $from, $send);
-    
+
     $message['headers']['Content-Type'] = 'text/html; charset=UTF-8; format=flowed';
     $message['headers']['From'] = $message['headers']['Sender'] = $message['headers']['Return-Path'] = $from;
     $message['subject'] = $subject;
     $message['body'] = $final_text;
-    
+
     // Retrieve the responsible implementation for this message.
     $system = drupal_mail_system($module, $key);
-    
+
     // Send e-mail.
     $message['result'] = $system->mail($message);
     // If no errors, let's add file access to the user.
     if ($message['result'] == 1) {
-      //getting user id from email.
+      // Getting user id from email.
       $users = db_query_range('SELECT uid FROM {users} WHERE mail = :mail', 0, 1, array(
-        ':mail' => $email
+        ':mail' => $email,
       ));
       // In the "impossible" case where two emails are the same in the db.
       $users = $users->fetchObject();
       $uid = $users->uid;
-      // actually adding the access to the user.
+      // Actually adding the access to the user.
       if (!is_null($uid) && isset($uid)) {
         foreach ($attachments as $file) {
           $query = db_insert('visualscience_uploaded_files')->fields(array(
@@ -71,12 +71,12 @@ class VisualscienceMessage
         }
       }
       echo '1';
-    } 
+    }
     else {
       echo '0';
     }
   }
-  
+
   /**
    * Sanitzes the content of an array recursively.
    * 
@@ -91,7 +91,7 @@ class VisualscienceMessage
     foreach ($un_safe_array as $entry) {
       if (gettype($entry) == 'array') {
         array_push($safe_array, $this->sanitizeArray($entry));
-      } 
+      }
       else {
         array_push($safe_array, check_plain($entry));
       }
