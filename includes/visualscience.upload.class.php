6c79ab4fa36327d60abf6712745edf1c2e8a8da9
Sébastien Arnold <seba-1511@2754171.no-reply.drupal.org>
2013-11-25 15:01:37 +0100

    Should be ok now with upload class.

--- includes/visualscience.upload.class.php
+++ includes/visualscience.upload.class.php
@@ -1,46 +1,46 @@
 <?php
 /**
  * @file
- * Provides everything needed for hendling upload and giving back files to users.
+ * Provides everything to handle upload and downloading files.
  */
-class visualscienceUpload {
+class VisualscienceUpload {
 
   /**
    * Creates the form to upload files to the server.
    * 
    * Page callback: Upload form
    *
-   * @param  form $form       
+   * @param form $form       
    *   Drupal
-   * @param  form $form_state 
+   * @param form $form_state 
    *   Drupal
    * 
    * @return array             
    *   Array used to generate the form
    */
-  public function visualscience_upload_form($form, &$form_state) {
+  public function visualscienceUploadForm($form, &$form_state) {
     $form['visualscience_upload_file'] = array(
       '#type' => 'file',
       '#size' => 20,
       '#required' => FALSE,
-      );
+    );
 
     $form['submit'] = array(
       '#type' => 'submit',
       '#value' => t('Save'),
-      );
+    );
 
-    $form['#submit'][] = 'visualscience_upload_submit';
+    $form['#submit'][] = 'visualscienceUploadSubmit';
     return $form;
   }
 
   /**
-   * Allows the download of a file to an authenticated and legitime user
+   * Allows the download of a file to an authenticated and legitime user.
    * @return none 
    *   Nothing
    */
-  public function visualscience_get_file_with_id() {
-    $file_id = doubleval($_GET['id']);
+  public function visualscienceGetFileWithId() {
+    $file_id = floatval($_GET['id']);
     global $user;
     global $base_url;
     $uid = $user->uid;
@@ -59,7 +59,7 @@ class visualscienceUpload {
           'Cache-Control' => 'must-revalidate',
           'Pragma' => 'no-cache',
           'Content-Length' => filesize($url),
-          );
+        );
         if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE')) {
           $headers['Cache-Control'] = 'must-revalidate, post-check=0, pre-check=0';
           $headers['Pragma'] = 'public';
@@ -67,30 +67,30 @@ class visualscienceUpload {
         file_transfer($url, $headers);
       }
       else {
-        drupal_set_message(t('You don\'t have the permission to access the file.'), 'error');
-        header( 'Location: ' . $base_url . '/visualscience/' );
+        drupal_set_message(t("You don't have the permission to access the file."), 'error');
+        header('Location: ' . $base_url . '/visualscience/');
       }
     }
     else {
       drupal_set_message(t('Please log in to access uploaded files.'), 'warning');
-      header( 'Location: ' . $base_url . '/user?destination=' . substr(request_uri(), strpos(request_uri(), '/visualscience/file') + 1) );
+      header('Location: ' . $base_url . '/user?destination=' . substr(request_uri(), strpos(request_uri(), '/visualscience/file') + 1));
     }
   }
 
   /**
    * Submits upload callback, register (db and server) file if needed.
    * 
-   * TODO: Change so that it can handle multiple uploads(Hint:Look for MultiUpload File Widget).
+   * TODO: Handle multiple uploads(Hint:Look for MultiUpload File Widget).
    * 
-   * @param  form $form       
+   * @param form $form       
    *   Drupal-generated
-   * @param  form $form_state 
+   * @param form $form_state 
    *   Drupal-generated
    * 
    * @return none             
    *   Nothing returned
    */
-  public function visualscience_upload_submit($form, &$form_state) {
+  public function visualscienceUploadSubmit($form, &$form_state) {
     $dir = 'private://';
     $validators = array();
     $file = 'visualscience_upload_file';
@@ -111,7 +111,7 @@ class visualscienceUpload {
         $result = db_query('SELECT * FROM {visualscience_uploaded_files} WHERE uid = :uid AND url = :url', array(':uid' => $uid, ':url' => $file->uri))->fetchObject();
         $id = $result->fid;
         $vs_url = drupal_get_path('module', 'visualscience');
-        if (strpos($vs_url, '?')) { 
+        if (strpos($vs_url, '?')) {
         // Handling the clean url problem.
           drupal_set_message(t('The file has been uploaded to: @url', array('@url' => $base_url . '/visualscience/file&id=' . $id)));
         }
